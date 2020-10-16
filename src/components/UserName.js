1e3d7f7e815c0e559362ef24153e1294ac646345
Rasha Zayed <zayed.rasha@outlook.com>
2020-10-15 23:53:18 -0700

    calling next url to get more pokemon

--- src/components/UserName.js
+++ src/components/UserName.js
@@ -1,39 +1,35 @@
-import React, {useEffect, useState} from'react';
+import React, { useEffect, useState } from "react";
 
-const GetUsersNames = props => {
-    const [user, setUser]= useState([]);
-    const [submitted , setSubmitted] = useState(false);
+const GetUsersNames = (props) => {
+  const [user, setUser] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
 
-    useEffect( ()=>{
-        console.log(submitted);
-        if (submitted){
-        fetch('https://pokeapi.co/api/v2/pokemon')
-            .then(response => response.json())
-            .then(response => setUser(response.results) )
-        }
-
-    } , [submitted]);
-
-    const display = user.map((item,i) =>{
-         return (
-            <div key={i}>{item.name}</div> 
-             )
-    });
-    const onClick = e => {
-        e.preventDefault();
-        setSubmitted(true);
+  useEffect(() => {
+    console.log(submitted);
+    if (submitted && user.length < 807) {
+      fetch(url)
+        .then((response) => response.json())
+        .then((response) => {
+          setUser([...user, ...response.results]);
+          setUrl(response.next);
+        });
     }
+  }, [submitted, url]);
 
+  const display = user.map((item, i) => {
+    return <div key={i}>{item.name}</div>;
+  });
+  const onClick = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
 
-
-
-
-    return (
-        <form >
-            <button onClick={onClick}>Fetch Pokemon</button>
-            {display}
-        </form>
-
-    )
-}
-export default GetUsersNames ;
+  return (
+    <form>
+      <button onClick={onClick}>Fetch Pokemon</button>
+      {display}
+    </form>
+  );
+};
+export default GetUsersNames;
